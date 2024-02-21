4ca425eceb0e28423a46648e7ea8982b469522f8
Fuzzbucket14 <159600680+Fuzzbucket14@users.noreply.github.com>
2024-02-21 09:04:14 -0600

    added more inputs

--- Main.java
+++ Main.java
@@ -3,9 +3,14 @@ import javax.swing.*;
 
 public class Main {
    public static void main(String[] args){
-    String name,adj0;
+
+    //creating strings
+    String name,adj0,color;
+
+    //prombting inputs
     name = JOptionPane.showInputDialog(null,"Enter a name", "title", 2);
-    adj0 = JOptionPane.showInputDialog(null,"Enter a adjective", "title", 2);
+    color = JOptionPane.showInputDialog(null,"Enter a color", "title", 2);
+    adj0 = (String)JOptionPane.showInputDialog(null,"Enter a adjective", "adjective",1,null,null, "big, small, kind, etc");
 
    }
 }
