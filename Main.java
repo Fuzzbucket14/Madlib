e32f73279e6118bd5dadbdf8597b2eff73e3c8ae
Fuzzbucket14 <159600680+Fuzzbucket14@users.noreply.github.com>
2024-02-21 08:55:04 -0600

    added adjective

--- Main.java
+++ Main.java
@@ -3,8 +3,9 @@ import javax.swing.*;
 
 public class Main {
    public static void main(String[] args){
-    String name;
-    name = JOptionPane.showInputDialog(null,"what is your name?", "title", 2);
+    String name,adj0;
+    name = JOptionPane.showInputDialog(null,"Enter a name", "title", 2);
+    adj0 = JOptionPane.showInputDialog(null,"Enter a adjective", "title", 2);
 
    }
 }
