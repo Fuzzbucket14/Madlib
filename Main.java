f0e019358a39a47e6305756dcbc0af5aac95a896
Fuzzbucket14 <159600680+Fuzzbucket14@users.noreply.github.com>
2024-02-21 09:11:13 -0600

    more inputs

--- Main.java
+++ Main.java
@@ -5,12 +5,14 @@ public class Main {
    public static void main(String[] args){
 
     //creating strings
-    String name,adj0,color;
+    String name,adj0,color,verb0;
 
     //prombting inputs
-    name = JOptionPane.showInputDialog(null,"Enter a name", "title", 2);
-    color = JOptionPane.showInputDialog(null,"Enter a color", "title", 2);
+    name = JOptionPane.showInputDialog(null,"Enter a name", "Name", 2);
+    color = JOptionPane.showInputDialog(null,"Enter a color", "Color", 2);
     adj0 = (String)JOptionPane.showInputDialog(null,"Enter a adjective", "adjective",1,null,null, "big, small, kind, etc");
-
+    
+    ImageIcon Smart = new ImageIcon("Smart.jpg");
+    verb0 = (String)JOptionPane.showInputDialog(null,"Enter a verb", "Verb",-1,Smart,null,null);
    }
 }
