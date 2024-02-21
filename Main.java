f42feff41f5c35267c132a92e55323e929d147cd
Fuzzbucket14 <159600680+Fuzzbucket14@users.noreply.github.com>
2024-02-21 08:52:49 -0600

    craeted main with a simple input box

---
+++ Main.java
@@ -0,0 +1,10 @@
+
+import javax.swing.*;
+
+public class Main {
+   public static void main(String[] args){
+    String name;
+    name = JOptionPane.showInputDialog(null,"what is your name?", "title", 2);
+
+   }
+}
